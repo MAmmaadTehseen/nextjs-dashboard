c3d391dba1bd0e02f18613d0338f423ff3b5d71f
M. Shaheer <tecxra1003@gmail.com>
2024-03-26 15:56:08 +0500

    partial rerendering

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -23,6 +23,12 @@ const customers = [
     image_url: '/customers/lee-robinson.png',
   },
   {
+    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+    name: 'Ammaad khaan',
+    email: 'ammad@gmail.com',
+    image_url: '/customers/lee-robinson.png',
+  },
+  {
     id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
     name: 'Hector Simpson',
     email: 'hector@simpson.com',
